073fe9df6ecca0da8c28614bd8d9c645538076fc
David G. Paul <jedi58@users.noreply.github.com>
2016-05-13 22:28:24 +0100

    Create example.js

---
+++ examples/2-MultiPlayers/example.js
@@ -0,0 +1,29 @@
+/**
+ * This will attach a media player to the element with ID `player_1`
+ * and associate it with two media files that will both be played
+ * when the play button is clicked. Paths can be specified to these files
+ */
+SimultaneousMediaPlayer.addPlayer('player_1', [
+    'example1.mp3',
+    'example2.mp3'
+]);
+
+/**
+ * This will attach a media player to the element with ID `player_2`
+ * and associate it a single media file that will be played
+ * when the play button is clicked. Paths can be specified to these files
+ */
+SimultaneousMediaPlayer.addPlayer('player_2', [
+    'example3.mp3'
+]);
+
+/**
+ * This will attach a media player to the element with ID `player_3`
+ * and associate it with three media files that will all be played
+ * when the play button is clicked. Paths can be specified to these files
+ */
+SimultaneousMediaPlayer.addPlayer('player_3', [
+    'example4.mp3',
+    'example5.mp3',
+    'example6.mp3'
+]);
